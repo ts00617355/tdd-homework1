efd45363bc999b0383d36cd442fc9f6e3da42be0
tinymao <winson58kimo@gmail.com>
2016-03-10 00:49:19 +0800

    complete itemSumByColumnAndNum logic

--- src/DataCalculator.php
+++ src/DataCalculator.php
@@ -16,6 +16,15 @@ class DataCalculator
      */
     public function itemSumByColumnAndNum($column, $itemNum)
     {
-        
+        // filter data by column
+        $dataFilterByColumn = array_column($this->data, $column, 'id');
+
+        // group the data by number
+        $dataGroup = array_chunk($dataFilterByColumn, $itemNum);
+
+        foreach ($dataGroup as $data) {
+            $result[] = array_sum($data);
+        }
+        return $result;
     }
 }
