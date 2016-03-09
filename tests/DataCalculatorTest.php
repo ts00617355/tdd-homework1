7b421edcf444ca4bbd5c2c5e9fc6c41d2584eceb
tinymao <winson58kimo@gmail.com>
2016-03-10 00:59:54 +0800

    test: column cost per 3

--- tests/DataCalculatorTest.php
+++ tests/DataCalculatorTest.php
@@ -19,4 +19,20 @@ class DataCalculatorTest extends PHPUnit_Framework_TestCase
         // assert
         $this->assertEquals($expected, $result);
     }
+
+    public function testItemSumByColumnAndNum_column_cost_per_3() {
+        // arrange
+        $fakeData = json_decode(file_get_contents(__DIR__ . '/../data.json'), true);
+        $column = 'cost';
+        $itemNum = 3;
+        $dataCalculator = new DataCalculator($fakeData);
+
+        $expected = [6, 15, 24, 21];
+
+        // act
+        $result = $dataCalculator->ItemSumByColumnAndNum($column, $itemNum);
+
+        // assert
+        $this->assertEquals($expected, $result);
+    }
 }
