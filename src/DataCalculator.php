631435433d65db654638b01d5b93d9ee233a4a02
tinymao <winson58kimo@gmail.com>
2016-03-10 00:44:25 +0800

    initial data calculator

---
+++ src/DataCalculator.php
@@ -0,0 +1,21 @@
+<?php
+namespace App;
+
+class DataCalculator
+{
+    private $data = [];
+
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * @param $column
+     * @param $itemNum
+     */
+    public function itemSumByColumnAndNum($column, $itemNum)
+    {
+        
+    }
+}
