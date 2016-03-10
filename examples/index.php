c7070893f1d1a7d4f5003d6b1768c1b4e858d6c2
tinymao <winson58kimo@gmail.com>
2016-03-10 10:49:47 +0800

    add examples

---
+++ examples/index.php
@@ -0,0 +1,15 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+use App\DataCalculator;
+
+$fakeData = json_decode(file_get_contents(__DIR__ . '/../data.json'), true);
+
+foreach ($fakeData as $data) {
+    echo $data['id'] . "\t" . $data['cost'] . "\t" . $data['revenue'] . "\t" . $data['sellPrice'] . PHP_EOL;
+}
+
+$dataCalcular = new DataCalculator($fakeData);
+print_r($dataCalcular->itemSumByColumnAndNum($argv[1], $argv[2]));
+
+
