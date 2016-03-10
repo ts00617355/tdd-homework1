a74c84b3fad4b04532423464a151369ed9a4dd43
tinymao <winson58kimo@gmail.com>
2016-03-10 10:52:10 +0800

    add titlebar

--- examples/index.php
+++ examples/index.php
@@ -5,6 +5,7 @@ use App\DataCalculator;
 
 $fakeData = json_decode(file_get_contents(__DIR__ . '/../data.json'), true);
 
+echo "id\tcost\trevenue\tsellPrice" . PHP_EOL;
 foreach ($fakeData as $data) {
     echo $data['id'] . "\t" . $data['cost'] . "\t" . $data['revenue'] . "\t" . $data['sellPrice'] . PHP_EOL;
 }
